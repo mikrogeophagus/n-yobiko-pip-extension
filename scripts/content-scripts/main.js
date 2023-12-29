fec69fd117de8f6570e2ef10571f9b3a9dd25ef8
Kaisei Yoneyama <96284126+Kaisei-Yoneyama@users.noreply.github.com>
2023-12-30 02:01:37 +0900

    運営コメントに対応

--- scripts/content-scripts/main.js
+++ scripts/content-scripts/main.js
@@ -1,6 +1,29 @@
 (async () => {
   'use strict'
 
+  const videoPlayer = await waitForSelector('[aria-label="動画プレイヤー"]')
+
+  //================================================
+  // 運営コメントの取得と設定
+  //================================================
+
+  // 表示中の運営コメントを保持する
+  let officialComment = ''
+
+  new MutationObserver((mutations, _observer) => {
+    for (const mutation of mutations) {
+      if (mutation.addedNodes.length) {
+        const addedNode = mutation.addedNodes.item(0)
+        officialComment = addedNode.textContent.trim()
+      } else if (mutation.removedNodes.length) {
+        officialComment = ''
+      }
+    }
+  }).observe(videoPlayer.firstElementChild, {
+    subtree: true,
+    childList: true
+  })
+
   //================================================
   // 映像レイヤーの取得と設定
   //================================================
@@ -37,6 +60,7 @@
 
         // 再生停止中にコメントの可視状態が変化した場合もフレームが更新されるようにする
         compositeLayers()
+        drawOfficialComment()
 
         // XXX: なぜか常にコメントレイヤーを非表示にしないと、映像レイヤーと合成できなかったり、コメントがチラついて表示されたりする
         //      コメント表示切替ボタンをクリックすると、コメントレイヤーの display: none が付け外しされてコメントの可視状態が切り替わっている
@@ -84,6 +108,25 @@
   }
 
   /**
+   * 運営コメントを描画する関数
+   * コメントが非表示の場合や PiP モードでない場合は描画しない
+   */
+  function drawOfficialComment() {
+    if (officialComment && isCommentsVisible && document.pictureInPictureElement) {
+      // 背景
+      context.fillStyle = 'rgba(46, 46, 71, 0.7)'
+      context.fillRect(0, height - 64, width, 64)
+
+      // テキスト
+      context.fillStyle = 'white'
+      context.textAlign = 'center'
+      context.textBaseline = 'middle'
+      context.font = 'bold 32px sans-serif'
+      context.fillText(officialComment, width / 2, height - 32)
+    }
+  }
+
+  /**
    * 映像レイヤーとコメントレイヤーを合成してアニメーションを再生する関数
    * 映像レイヤーが再生されていない場合は終了する
   */
@@ -91,6 +134,7 @@
     if (sourceVideo.paused || sourceVideo.ended) return
     requestAnimationFrame(animate)
     compositeLayers()
+    drawOfficialComment()
   }
 
   //================================================
@@ -109,6 +153,19 @@
   // 再生停止中にシーク操作をした場合もフレームが更新されるようにする
   sourceVideo.addEventListener('seeked', (_event) => {
     compositeLayers()
+    drawOfficialComment()
+  })
+
+  // 再生停止中に PiP モードを変更した場合もフレームが更新されるようにする
+  video.addEventListener('enterpictureinpicture', (_event) => {
+    compositeLayers()
+    drawOfficialComment()
+  })
+
+  // 再生停止中に PiP モードを変更した場合もフレームが更新されるようにする
+  video.addEventListener('leavepictureinpicture', (_event) => {
+    compositeLayers()
+    drawOfficialComment()
   })
 
   //================================================
